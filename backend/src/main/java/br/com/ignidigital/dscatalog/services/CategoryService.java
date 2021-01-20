cf60a341c8f3c8e35b35badb18f27b10ca55d15d
Jackson Rodrigues <jjr0910@gmail.com>
2021-01-20 16:03:42 -0300

    Database seeding

--- backend/src/main/java/br/com/ignidigital/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/ignidigital/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import br.com.ignidigital.dscatalog.entities.Category;
 import br.com.ignidigital.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,7 +13,8 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-
+    
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
