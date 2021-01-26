3c73d8cc6a52622900d81004c581b5a3861f7deb
Jackson Rodrigues <jjr0910@gmail.com>
2021-01-26 20:22:40 -0300

    Product entity, SQL script

--- backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO TB_CATEGORY (NAME, created_At) VALUES ('Books', NOW());
-INSERT INTO TB_CATEGORY (NAME, created_At) VALUES ('Electronics', NOW());
-INSERT INTO TB_CATEGORY (NAME, created_At) VALUES ('Computers', NOW());
