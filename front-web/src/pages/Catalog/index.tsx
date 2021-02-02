69fe8eb3788c666b0f057296b4c4a6a1d2ee57bf
Jackson Rodrigues <jjr0910@gmail.com>
2021-02-01 22:54:21 -0300

    Product listing layout

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () => (
@@ -7,7 +8,17 @@ const Catalog = () => (
             Catálogo de produtos
          </h1>
          <div className="catalog-products">
-
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
+           <ProductCard />
          </div>
     </div>
 );
