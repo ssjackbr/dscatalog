519866cb717b3011ee06559d9e2cd8ced03385ba
Jackson Rodrigues <jjr0910@gmail.com>
2021-05-14 19:06:08 -0300

    ORM mapping, database seeding

--- backend/src/main/java/br/com/ignidigital/dscatalog/entities/Category.java
+++ backend/src/main/java/br/com/ignidigital/dscatalog/entities/Category.java
@@ -3,7 +3,9 @@ package br.com.ignidigital.dscatalog.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -20,6 +22,9 @@ public class Category implements Serializable {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updateAt;
+
+    @ManyToMany (mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -43,6 +48,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     public Instant getCreatedAt() {
--- backend/src/main/java/br/com/ignidigital/dscatalog/entities/Product.java
+++ backend/src/main/java/br/com/ignidigital/dscatalog/entities/Product.java
@@ -26,8 +26,11 @@ public class Product implements Serializable {
     private Instant date;
 
     @ManyToMany
-    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn (name = "product_id"), inverseJoinColumns = @JoinColumn (name = "category_id"))
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn (name = "product_id"),
+            inverseJoinColumns = @JoinColumn (name = "category_id"))
     Set<Category> categories = new HashSet<>();
+
 
     /* Auditoria
 
