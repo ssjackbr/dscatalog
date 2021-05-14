114430d5bbc248d5c171e008b03751d5d8e02f66
Jackson Rodrigues <jjr0910@gmail.com>
2021-05-14 19:08:41 -0300

    ORM mapping, database seeding

--- backend/src/main/java/br/com/ignidigital/dscatalog/entities/User.java
+++ backend/src/main/java/br/com/ignidigital/dscatalog/entities/User.java
@@ -20,9 +20,8 @@ public class User implements Serializable {
     private String password;
 
     @ManyToMany
-    @JoinTable(name = "tb_user_role",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"),
+              inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
